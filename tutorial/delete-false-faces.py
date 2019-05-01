2d15b72eebd6c1b43b2be99d577d2c2b8ee88158
Marc Püls <mpuels@gmail.com>
2019-05-01 23:40:26 +0200

    Add script to delete images that contain no face
    List of images has been created manually.

---
+++ tutorial/delete-false-faces.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import glob
+from pathlib import Path
+
+FACES_DIR = Path("~/data/face").expanduser()
+FALSE_FACES_TXT = "false-faces.txt"
+
+
+def main():
+    png_name_to_path = {}
+    for png in FACES_DIR.glob('**/*.png'):
+        png_name_to_path[png.name] = png
+
+    with open(FALSE_FACES_TXT) as f:
+        false_face_png_names = [line.strip() for line in f]
+
+    for false_face_png_name in false_face_png_names:
+        if false_face_png_name in png_name_to_path:
+            png_to_delete = png_name_to_path[false_face_png_name]
+            print(f"Deleting {png_to_delete}")
+            png_to_delete.unlink()
+        else:
+            print(f"File {false_face_png_name} does not exist.")
+
+
+if __name__ == "__main__":
+    main()
