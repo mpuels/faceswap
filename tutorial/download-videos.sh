d65da01518308dc6bedbdb996c4792f684f1a5b4
Marc Püls <mpuels@gmail.com>
2019-05-02 13:48:12 +0200

    In upload and download scripts: do 'sync' instead of 'copy'

--- tutorial/download-videos.sh
+++ tutorial/download-videos.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 DATA_DIR=~/data
 S3_BUCKET_NAME=gifswap-39485849
 
-aws s3 cp --recursive s3://${S3_BUCKET_NAME}/video ${DATA_DIR}/video
+aws s3 sync s3://${S3_BUCKET_NAME}/video ${DATA_DIR}/video
