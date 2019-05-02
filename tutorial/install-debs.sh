c847ce2f01014f2c1fcfd555647989933c9844df
Marc Püls <mpuels@gmail.com>
2019-05-02 12:56:54 +0200

    fix: install cmake twice (otherwise command 'cmake' is not available)

--- tutorial/install-debs.sh
+++ tutorial/install-debs.sh
@@ -3,3 +3,8 @@
 set -euo pipefail
 
 sudo apt install cmake ffmpeg
+
+# Oddly we have to remove and re-install cmake. Otherwise the command 'cmake'
+# is not available on the command line.
+sudo apt remove cmake
+sudo apt install cmake
