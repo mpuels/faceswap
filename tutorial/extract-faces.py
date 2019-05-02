780672877b5238534df0ebe7c555c516d7e15f24
Marc Püls <mpuels@gmail.com>
2019-05-02 13:36:55 +0200

    Rewrite script to extracte faces in Python

---
+++ tutorial/extract-faces.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import subprocess
+
+
+DATA_DIR = Path("~/data").expanduser()
+NAMES = [
+    "cage",
+    "marcpuels",
+    "trump",
+]
+
+
+def main():
+    for name in NAMES:
+        img_dir = DATA_DIR / "img" / name
+        face_dir = DATA_DIR / "face" / name
+        subprocess.run(["python", "../faceswap.py", "extract",
+                        "-i", str(img_dir),
+                        "-o", str(face_dir)])
+
+if __name__ == "__main__":
+    main()
