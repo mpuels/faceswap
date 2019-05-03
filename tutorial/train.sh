fcaaab45a084e274f74707c00e99ac54b76008be
Marc Püls <mpuels@gmail.com>
2019-05-03 16:42:28 +0200

    train.sh: Set batch size and log level

--- tutorial/train.sh
+++ tutorial/train.sh
@@ -4,13 +4,18 @@ set -euo pipefail
 
 DATA_DIR=~/data
 
-CAGE_FACE_DIR=${DATA_DIR}/face/cage
-TRUMP_FACE_DIR=${DATA_DIR}/face/trump
+NAME_A=trump
+NAME_B=marcpuels
 
-MODEL_DIR=${DATA_DIR}/models
+MODEL_DIR=${DATA_DIR}/models/${NAME_A}-${NAME_B}
 
 pushd ..
 
-python faceswap.py train -A ${TRUMP_FACE_DIR} -B ${CAGE_FACE_DIR} -m ${MODEL_DIR}
+python faceswap.py train \
+       --batch-size 128 \
+       -L TRACE \
+       -A ${DATA_DIR}/face/${NAME_A} \
+       -B ${DATA_DIR}/face/${NAME_B} \
+       -m ${MODEL_DIR}
 
 popd
