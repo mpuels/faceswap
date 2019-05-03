d01d6413968330004d39f906efe63520342b37c6
Marc Püls <mpuels@gmail.com>
2019-05-03 16:41:35 +0200

    convert.sh: Replace literals with variables

--- tutorial/convert.sh
+++ tutorial/convert.sh
@@ -4,6 +4,15 @@ set -euo pipefail
 
 pushd ..
 
-python faceswap.py convert -i ~/data/img/trump -o ~/data/swaps/cage-for-trump/ -m ~/data/models/
+DATA_DIR=~/data
+FACE_TO_REPLACE=trumpsample
+FACE_TO_INSERT=marcpuels2
+
+MODEL=trump-marcpuels
+
+python faceswap.py convert \
+       -i ${DATA_DIR}/img/${FACE_TO_REPLACE} \
+       -o ${DATA_DIR}/swaps/${FACE_TO_INSERT}-for-${FACE_TO_REPLACE}/ \
+       -m ${DATA_DIR}/models/${MODEL}
 
 popd
