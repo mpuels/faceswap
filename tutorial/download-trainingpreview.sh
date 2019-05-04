558d158f4d9cccb0b1e95e90731a5220f84c132a
Marc Püls <mpuels@gmail.com>
2019-05-05 01:15:52 +0200

    download-trainingpreviews.sh: Sync folder instead of copying file
    The GPU server now uploads the preview image every hour and includes the
    timestamp in the preview's file name.

--- tutorial/download-trainingpreview.sh
+++ tutorial/download-trainingpreview.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 . config.sh
 
-aws s3 cp s3://${S3_BUCKET_NAME}/training_preview.jpg ${DATA_DIR}/training_preview.jpg
+aws s3 sync s3://${S3_BUCKET_NAME}/training_previews ${DATA_DIR}/training_previews
