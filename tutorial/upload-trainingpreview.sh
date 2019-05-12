238b31043683b8c802922c4bbfac30d26983a4f4
Marc Püls <mpuels@gmail.com>
2019-05-12 23:07:39 +0200

    upload-trainingpreviews.sh: Append timestamp before uploading image

--- tutorial/upload-trainingpreview.sh
+++ tutorial/upload-trainingpreview.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 
 . config.sh
 
-aws s3 cp ../training_preview.jpg s3://${S3_BUCKET_NAME}/training_preview.jpg
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+NOW=$(date -u +%Y-%m-%d-%H-%M-%S)
+aws s3 cp ${DIR}/../training_preview.jpg \
+       s3://${S3_BUCKET_NAME}/training_previews/training_preview-${NOW}.jpg
