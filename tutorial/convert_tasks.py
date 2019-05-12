c9c9f7ca9c0a842f34c66989789aae2183cd373d
Marc Püls <mpuels@gmail.com>
2019-05-12 23:02:24 +0200

    Add convert tasks: marcpuels, marcpuels_sample, marcpuels-for-trumpsample,
    marcpuels_nachricht_an_alex_sample,
    marcpuels_nachricht_an_alex_sample_trump

--- tutorial/convert_tasks.py
+++ tutorial/convert_tasks.py
@@ -1,5 +1,7 @@
 from typing import NamedTuple
 
+from config import IMG_DIR
+
 
 class ConvertTask(NamedTuple):
     img_dir: str
@@ -8,7 +10,44 @@ class ConvertTask(NamedTuple):
 
 
 CONVERT_TASKS = {
-    "marcpuels-for-trumpsample": ConvertTask("trumpsample",
-                                             "trump-marcpuels",
-                                             [])
+    "marcpuels": ConvertTask(
+        "marcpuels",
+        "marcpuels-janboehmermann",
+        [
+            "--alignments", str(IMG_DIR / "marcpuels" / "alignments.json"),
+            "--loglevel", "TRACE",
+            # "--singleprocess",
+        ]
+    ),
+    "marcpuels_sample": ConvertTask(
+        "marcpuels_sample",
+        "marcpuels-janboehmermann",
+        [
+            "--alignments", str(IMG_DIR / "marcpuels" / "alignments.json"),
+            "--loglevel", "TRACE",
+            # "--singleprocess",
+        ]
+    ),
+    "marcpuels-for-trumpsample": ConvertTask(
+        "trumpsample",
+        "trump-marcpuels",
+        []),
+    "marcpuels_nachricht_an_alex_sample": ConvertTask(
+        "marcpuels_nachricht_an_alex_sample",
+        "marcpuels-janboehmermann",
+        [
+            # "--alignments", str(IMG_DIR / "marcpuels_nachricht_an_alex" / "alignments.json"),
+            # "--swap-model",
+            "--loglevel", "TRACE",
+            "--singleprocess",
+        ]),
+    "marcpuels_nachricht_an_alex_sample_trump": ConvertTask(
+        "marcpuels_nachricht_an_alex_sample",
+        "trump-marcpuels",
+        [
+            # "--alignments", str(IMG_DIR / "marcpuels_nachricht_an_alex" / "alignments.json"),
+            # "--swap-model",
+            "--loglevel", "TRACE",
+            "--singleprocess",
+        ]),
 }
