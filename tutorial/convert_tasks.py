0ab929512676a4e629645d32133fae081a242d78
Marc Püls <mpuels@gmail.com>
2019-05-23 22:49:11 +0200

    convert_tasks.py: Only keep 'convert_args' in ConvertTask

--- tutorial/convert_tasks.py
+++ tutorial/convert_tasks.py
@@ -1,53 +1,80 @@
 from typing import NamedTuple
 
-from config import IMG_DIR
+from config import IMG_DIR, MODEL_DIR, VIDEO_DIR
 
 
 class ConvertTask(NamedTuple):
-    img_dir: str
-    model: str
     convert_args: list
 
 
+# ffmpeg -i marcpuelssample-001.mpg -vf scale=-1:480 marcpuelssample-001-480.mpg
+
 CONVERT_TASKS = {
-    "marcpuels": ConvertTask(
-        "marcpuels",
-        "marcpuels-janboehmermann",
+    "marcpuels"                                 : ConvertTask(
         [
-            "--alignments", str(IMG_DIR / "marcpuels" / "alignments.json"),
+            "--input-dir", IMG_DIR / "marcpuels",
+            "--alignments", IMG_DIR / "marcpuels" / "alignments.json",
             "--loglevel", "TRACE",
+            "--model", MODEL_DIR / "marcpuels-janboehmermann",
             # "--singleprocess",
+        ]),
+    "marcpuelssample-001-video"                 : ConvertTask(
+        [
+            "--input-dir", IMG_DIR / "marcpuels-sample",
+            "--reference-video", VIDEO_DIR / "marcpuels-sample" / "marcpuelssample-001.mpg",
+            "--model", MODEL_DIR / "marcpuels-janboehmermann_villain",
+            "--writer", "ffmpeg",
         ]
     ),
-    "marcpuels_sample": ConvertTask(
-        "marcpuels_sample",
-        "marcpuels-janboehmermann",
+    "marcpuelssample-002-video"                 : ConvertTask(
         [
-            "--alignments", str(IMG_DIR / "marcpuels" / "alignments.json"),
-            "--loglevel", "TRACE",
-            # "--singleprocess",
+            "--input-dir", IMG_DIR / "marcpuels-sample-002",
+            "--reference-video", VIDEO_DIR / "marcpuels-sample-002" / "marcpuelssample-002.avi",
+            "--model", MODEL_DIR / "marcpuels-janboehmermann_villain",
+            "--writer", "ffmpeg",
         ]
     ),
-    "marcpuels-for-trumpsample": ConvertTask(
-        "trumpsample",
-        "trump-marcpuels",
-        []),
-    "marcpuels_nachricht_an_alex_sample": ConvertTask(
-        "marcpuels_nachricht_an_alex_sample",
-        "marcpuels-janboehmermann",
+    "marcpuels-sample-001-480"                  : ConvertTask(
         [
-            # "--alignments", str(IMG_DIR / "marcpuels_nachricht_an_alex" / "alignments.json"),
-            # "--swap-model",
-            "--loglevel", "TRACE",
-            "--singleprocess",
-        ]),
-    "marcpuels_nachricht_an_alex_sample_trump": ConvertTask(
-        "marcpuels_nachricht_an_alex_sample",
-        "trump-marcpuels",
+            '--input-dir', IMG_DIR / "marcpuels-sample-001-480",
+            '--reference-video', VIDEO_DIR / "marcpuels-sample-001-480" / 'marcpuelssample-001-480.mpg',
+            '--alignments', IMG_DIR / "marcpuels-sample-001-480" / 'alignments.json',
+            '--model', MODEL_DIR / "marcpuels-janboehmermann_villain",
+            '--writer', 'ffmpeg'
+        ]
+    ),
+    "marcpuels_sample"                          : ConvertTask(
         [
-            # "--alignments", str(IMG_DIR / "marcpuels_nachricht_an_alex" / "alignments.json"),
-            # "--swap-model",
-            "--loglevel", "TRACE",
-            "--singleprocess",
-        ]),
+            "--input-dir", IMG_DIR / "marcpuels_sample",
+            "--model", MODEL_DIR / "marcpuels-janboehmermann",
+            "--alignments", IMG_DIR / "marcpuels" / "alignments.json",
+        ]
+    ),
+    "marcpuels-for-trumpsample"                 : ConvertTask(
+        [
+            "--input-dir", IMG_DIR / "trumpsample",
+            "--model", MODEL_DIR / "trump-marcpuels",
+        ]
+    ),
+    "marcpuels_nachricht_an_alex_sample"        : ConvertTask(
+        [
+            "--input-dir", IMG_DIR / "marcpuels_nachricht_an_alex_sample",
+            "--model", MODEL_DIR / "marcpuels-janboehmermann",
+            "--alignments", IMG_DIR / "marcpuels_nachricht_an_alex" / "alignments.json",
+        ]
+    ),
+    "marcpuels_nachricht_an_alex_sample-villain": ConvertTask(
+        [
+            "--input-dir", IMG_DIR / "marcpuels_nachricht_an_alex_sample",
+            "--model", MODEL_DIR / "marcpuels-janboehmermann_villain",
+            "--alignments", IMG_DIR / "marcpuels_nachricht_an_alex" / "alignments.json",
+        ]
+    ),
+    "marcpuels_nachricht_an_alex_sample_trump"  : ConvertTask(
+        [
+            "--input-dir", IMG_DIR / "marcpuels_nachricht_an_alex_sample",
+            "--model", "rump-marcpuels",
+            "--alignments", IMG_DIR / "marcpuels_nachricht_an_alex" / "alignments.json",
+        ]
+    )
 }
